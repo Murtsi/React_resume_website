545dd7f427b1abd0d34f8b329125839d60ce05e4
Murtsi <antti.murtsi@gmail.com>
2024-07-09 12:15:27 +0300

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { useEffect } from 'react'
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
